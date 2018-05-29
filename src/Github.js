1b9547777d7686a58cb1fd12430edacaa609d387
Luke Gregurich <lgreguri@purdue.edu>
2018-05-29 11:09:38 -0400

    add submit handler to add github user to the URL

--- src/Github.js
+++ src/Github.js
@@ -12,6 +12,12 @@ class Github extends Component {
         this.setState({username: ev.target.value})
     }
 
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+        this.setState({ username: ''}) 
+    }
+
     render() {
         return (
         <div className="Github">
@@ -20,17 +26,17 @@ class Github extends Component {
                 src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
                 alt="GitHub"
             />
-            <form>
-            <div>
-                <input 
-                    type="text"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                />
-            </div>
-            <div>
-                <button type="submit">Look up GitHub user</button>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input 
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Look up GitHub user</button>
+                </div>
             </form>
         </div>
         )
